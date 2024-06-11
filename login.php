6fd1eb7524b41c3c58995a7f9a46013ed658b1c8
FARAZIFTAN <mahasiswaulbiit@gmail.com>
2024-06-11 12:51:05 +0700

    update login

--- login.php
+++ login.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    <link rel="stylesheet" href="style_login.css"> <!-- Ganti dengan path CSS Anda -->
+    <link rel="stylesheet" href="CSS/style_login.css"> <!-- Ganti dengan path CSS Anda -->
 </head>
 
 <body>
